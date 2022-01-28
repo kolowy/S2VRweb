bcf775d28c50ce9bd2f3c2e2e9fd7bd896c2bad5
kolowy <paul.paul.dufour@gmail.com>
2022-01-28 17:23:57 +0100

    [FIX] wave + header responsive

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -6,9 +6,10 @@ class Footer extends Component {
     render() {
         return (
             <footer className="App-footer">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#3B4252" fill-opacity="1" d="M0,224L60,229.3C120,235,240,245,360,261.3C480,277,600,299,720,288C840,277,960,235,1080,218.7C1200,203,1320,213,1380,218.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-</svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 260">
+                    <path fill="#3B4252" fill-opacity="1" d="M0,256L120,229.3C240,203,480,149,720,149.3C960,149,1200,203,1320,229.3L1440,256L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+                </svg>
+
                 <div className='App-footer-copyright'> 
                     <div className='App-footer-name'>
                         <p>Made with <b>&hearts;</b> by hand</p>
