5d1cdc563a146d99b92f39155f0ed4e7c2651ecf
kolowy <paul.paul.dufour@gmail.com>
2022-03-06 18:33:06 +0100

    [FIX] author

--- src/js/pages/Author.js
+++ src/js/pages/Author.js
@@ -14,7 +14,6 @@ class Author extends Component {
                             title={"Project leader, programmer, tester"}
                             description={"Manages the overall state of the Unity project and its integration with VR software. Has the hardware capable of running VR."}
                             login={"auguste.charpentier"}
-                            email={"auguste.charpentier@epita.fr"}
                             img={"https://photos.cri.epita.fr/auguste.charpentier"}
                         />
                         <Card
@@ -22,7 +21,6 @@ class Author extends Component {
                             title={"Artistic direction, programmer"}
                             description={"In charge of the artistic direction, will provide design ideas and artworks. Also will contribute to the code."}
                             login={"david.goncalves"}
-                            email={"david.goncalves@epita.fr"}
                             img={"https://photos.cri.epita.fr/david.goncalves"}
                         />
                         <Card
@@ -30,7 +28,6 @@ class Author extends Component {
                             title={"3D modeler, programmer"}
                             description={"Has the most experience with 3D modeling and Unity tools like animations, shaders and other various stuff."}
                             login={"julie.fiadino"}
-                            email={"julie.fiadino@epita.fr"}
                             img={"https://photos.cri.epita.fr/julie.fiadino"}
                         />
                         <Card
@@ -38,7 +35,6 @@ class Author extends Component {
                             title={"Web developer, Programmer"}
                             description={"Will manage the website of the project and contribute to the code."}
                             login={"paul.dufour"}
-                            email={"paul.dufour@epita.fr"}
                             img={"https://photos.cri.epita.fr/paul.dufour"}
                         />
                     </div>
